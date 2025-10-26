a490cdcbbe82e5d773223363ca748fb91f00bdd1
gabriel <onlinehassle1234@gmail.com>
2025-10-26 18:09:10 +0100

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@next/next/no-img-element": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
 ];
