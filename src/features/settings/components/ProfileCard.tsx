83a02856501bd626bff9447fecb4b3304a8d102b
gabriel <onlinehassle1234@gmail.com>
2026-03-29 13:26:22 +0100

    refactor: reorganize ProfileCard layout and enhance button functionality

--- src/features/settings/components/ProfileCard.tsx
+++ src/features/settings/components/ProfileCard.tsx
@@ -8,7 +8,6 @@ import {
   Sparkles,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
-import ProfileManagementModal from "./ProfileManagementModal";
 import { ResumeAggregate } from "@/shared/types/resume.types";
 import { formatAppliedDate } from "@/lib/utils/helpers";
 // import { useDeleteResumeMutation } from "@/features/resume/mutations/resume.mutations";
@@ -30,45 +29,8 @@ export const ProfileCard: React.FC<{
     <div
       className={cn(
         "bg-white rounded-lg font-inter border border-gray-200  mb-4 relative",
-        //  isDefault && "shadow-xl shadow-blue-100"
       )}
     >
-      <div className="absolute top-4 grid grid-cols-3 gap-2 right-2 ">
-        <Button
-          onClick={() => {
-            download(resume.id, resume.fileName ?? resume.title ?? "document");
-          }}
-          variant={"ghost"}
-          className=""
-        >
-          {isDownloading ? (
-            <Loader className="size-4 animate-spin" />
-          ) : (
-            <Download className="w-4 h-4" />
-          )}
-        </Button>
-        <Button
-          onClick={() => {
-            preview(resume.id);
-          }}
-          variant={"ghost"}
-          className=""
-        >
-          {isPreviewing ? (
-            <Loader className="size-4 animate-spin" />
-          ) : (
-            <Eye className="w-4 h-4" />
-          )}
-        </Button>
-        <Button
-          onClick={() => {
-            handleEditClick(true);
-          }}
-          className=""
-        >
-          <Edit className="w-4 h-4" />
-        </Button>
-      </div>
       {editResume ? (
         <CreateUserResume
           resumeId={resume.id}
@@ -76,7 +38,7 @@ export const ProfileCard: React.FC<{
           handleEditClick={handleEditClick}
         />
       ) : (
-        <ProfileManagementModal resume={resume}>
+        <div>
           <div>
             <div className="p-6">
               <div className="flex items-center mb-4">
@@ -107,14 +69,87 @@ export const ProfileCard: React.FC<{
                 {formatAppliedDate(resume.createdAt)}
               </p>
             </div>
+
+            <div className="hidden sm:absolute sm:top-4 sm:grid grid-cols-3 gap-2 right-2 mr-2">
+              <Button
+                onClick={() => {
+                  download(
+                    resume.id,
+                    resume.fileName ?? resume.title ?? "document",
+                  );
+                }}
+                variant={"ghost"}
+                className=""
+              >
+                {isDownloading ? (
+                  <Loader className="size-4 animate-spin" />
+                ) : (
+                  <Download className="w-4 h-4" />
+                )}
+              </Button>
+              <Button
+                onClick={() => {
+                  preview(resume.id);
+                }}
+                variant={"ghost"}
+                className=""
+              >
+                {isPreviewing ? (
+                  <Loader className="size-4 animate-spin" />
+                ) : (
+                  <Eye className="w-4 h-4" />
+                )}
+              </Button>
+              <Button
+                onClick={() => {
+                  handleEditClick(true);
+                }}
+                className=""
+              >
+                <Edit className="w-4 h-4" />
+              </Button>
+            </div>
             <div className="border-t p-4">
-              <button className="flex items-center justify-between w-full text-blue-600 text-sm font-medium hover:text-blue-700 transition-colors">
-                <span>Edit Profile</span>
-                <ChevronRight className="w-4 h-4" />
-              </button>
+              <div className="flex gap-4 justify-between items-center">
+                <button className="flex items-center justify-between text-blue-600 text-sm font-medium hover:text-blue-700 transition-colors">
+                  <span>Edit Profile</span>
+                </button>
+                <div className="flex gap-2">
+                  <Button
+                    onClick={() => {
+                      download(
+                        resume.id,
+                        resume.fileName ?? resume.title ?? "document",
+                      );
+                    }}
+                    // variant={"ghost"}
+                    className="sm:hidden"
+                  >
+                    {isDownloading ? (
+                      <Loader className="size-4 animate-spin" />
+                    ) : (
+                      <Download className="w-4 h-4" />
+                    )}
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      preview(resume.id);
+                    }}
+                    // variant={"ghost"}
+                    className="sm:hidden"
+                  >
+                    {isPreviewing ? (
+                      <Loader className="size-4 animate-spin" />
+                    ) : (
+                      <Eye className="w-4 h-4" />
+                    )}
+                  </Button>
+                  <ChevronRight className="size-6 text-blue-500 max-sm:hidden" />
+                </div>
+              </div>
             </div>
           </div>
-        </ProfileManagementModal>
+        </div>
       )}
       {error && (
         <div className="mt-2.5 p-4 text-[12px] font-medium text-red-500">
